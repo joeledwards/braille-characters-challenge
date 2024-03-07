5c873a86d3a6e432ab26f4b89ab83b1933b2e49c
Joel Edwards <joeledwards@gmail.com>
2024-03-07 09:15:12 -0700

    feat: added a comment explaining the reference solution

--- reference.js
+++ reference.js
@@ -1,28 +1,94 @@
 
-// Minor (vertical) divisions are every 8 values
-// Major (horizontal) divisions are every 64 values
-//
-// LSB to MSB Ordering:
-// * Low / Mid --> top to bottom
-// * High -------> left to right
-//
-// 3 Low    3 Mid    2 High
-//  --       --       --
-// |o |     | o|     |  |
-// |o |     | o|     |  |
-// |o |     | o|     |  |
-// |  |     |  |     |oo|
-//  --       --       --
-//
-//  Coordinates of bits in order of least to most significant:
-//  (0, 3) => 1
-//  (0, 2) => 2
-//  (0, 1) => 4
-//  (1, 3) => 8
-//  (1, 2) => 16
-//  (1, 1) => 32
-//  (0, 0) => 64
-//  (1, 0) => 128
+/**
+ * There are 256 distinct charcters representing all of the possible 
+ * permutations of visible/invisible dots in the 4x2 grid. These are 
+ * represented by unicode codepoints 0x2800 - 0x28ff. Masking off the
+ * base value of 0x2800, we get a mapping from 0x00 to 0xff
+ * (0 - 255 decimal). If you look at how each of the "bits" change based
+ * on the associated, masked value, you can see that they behave exactly
+ * like the bits in a byte. The position of each bit has to be determined
+ * in order to correctly translate from one ordering to another in
+ * the various mapper functions that drive each of the solutions.
+ *
+ * LSB to MSB Ordering:
+ * * Low / Mid --> top to bottom
+ * * High -------> left to right
+ *
+ * 3 Low    3 Mid    2 High
+ *  --       --       --
+ * |o |     | o|     |  |
+ * |o |     | o|     |  |
+ * |o |     | o|     |  |
+ * |  |     |  |     |oo|
+ *  --       --       --
+ *
+ *  Coordinates of bits in order of least to most significant:
+ *  (0, 3) => 1
+ *  (0, 2) => 2
+ *  (0, 1) => 4
+ *  (1, 3) => 8
+ *  (1, 2) => 16
+ *  (1, 1) => 32
+ *  (0, 0) => 64
+ *  (1, 0) => 128
+ *
+ *
+ * Native:
+ *
+ * For the native order mapper, we can just translate every value (0 - 255)
+ * into the appropriate codepoint by ORing (adding) the base value with
+ * the value:
+ *
+ * 0x2800 | 0x002f => 0x282f
+ *
+ *
+ *
+ * Horizontal/Vertical:
+ *
+ * For the other mappings, we need to translate the position of the bits.
+ * Turns out this is as simple as re-mapping the values associated with
+ * each bit. Since the position of each bit is stable, all we have to do
+ * is create a mapping between the bits of the input value and the bits
+ * of the desired output location of each of those bits.
+ *
+ * For example, let's say we want to map bit 2 to the correct location
+ * for vertical orientation. Since bit 2 is at position (0, 2) for both
+ * the native and the vertical orientation, nothing needs to change. So
+ * bit 2 retains its value of 2.
+ *
+ * 2 Native    2 Vertical
+ *  --          -- 
+ * |  |        |  |
+ * |o |        |o |
+ * |  |        |  |
+ * |  |        |  |
+ *  --          -- 
+ *
+ * However, when we want to map bit 2 to the correct location for
+ * horizontal orientation, we need to adjust it to match the value
+ * for the bit in that same position in the native orientation. This
+ * means we have to map bit 2 to a value of 8. This is because we
+ * want bit 2 to be placed at (1, 3), and the bit value at that
+ * location in native orientation is 8.
+*
+ * 2 Native    2 Horizontal
+ *  --          -- 
+ * |  |        | o|
+ * |o |        |  |
+ * |  |        |  |
+ * |  |        |  |
+ *  --          -- 
+ *
+ * Using this same approach for all bits, we just need to build a map
+ * between bit values for each orientation. This maps are contained in
+ * the `horizontalBinaryOrder` and `verticalBinaryorder` variables.
+ *
+ * Then we can do a lookup in the bit map for each bit in the incoming value,
+ * then combine all of those into the native value containing the desired bit
+ * sequence. This is still a value between 0 and 255. Later we convert this into
+ * a codepoint and then a character. This lookup and combination logic is
+ * located in the remap() function.
+ */
 
 const baseValue = 0x2800
 
